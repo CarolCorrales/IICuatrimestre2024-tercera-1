7b7549af6f359b1f38fdf01742c2905b025ed1c7
Carolina <carol.cch97@gmail.com>
2024-06-12 21:47:46 -0600

    Primera_clase

---
+++ parte_superior.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="English">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="Proyecto de tercera 1" content="Pagina de prueba">
+        <title >
+            Tercera 1
+        </title>
+        <link rel="stylesheet" href="./styles/main.css">
+
+
+    </head>
+
+    <menu>
+        <div class="menu-izquierda">
+            <a href="https://index.php" > <img src=".images/menu.png" alt="">
+
+
+        </a>
+
+
+            
+
+
+
+
+
+        </div>
+
+
+
+    </menu>
+
+</html>
